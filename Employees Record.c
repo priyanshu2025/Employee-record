b652d0793ae1e05bed02a47f5e9634d5077068d1
priyanshu2025 <83900383+priyanshu2025@users.noreply.github.com>
2021-05-23 17:28:38 +0530

    Employees Records

---
+++ Employees Record.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct{
+ 
+    char name[30];
+    int age;
+    int salary;
+ 
+} Employee;
+ 
+int main()
+{
+    int i, n=20;
+ 
+    Employee employees[n];
+ 
+    //Taking each employee detail as input
+ 
+    printf("Enter %d Employee Details \n \n",n);
+    for(i=0; i<n; i++){
+ 
+        printf("Employee %d:- \n",i+1);
+        //Name
+        printf("Name: ");
+        scanf("%s",employees[i].name);
+        //ID
+        printf("Age: ");
+        scanf("%d",&employees[i].age);
+        //Salary
+        printf("Salary: ");
+        scanf("%d",&employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    //Displaying Employee details
+ 
+    printf("-------------- All Employees Details ---------------\n");
+ 
+    for(i=0; i<n; i++){
+ 
+        printf("Name \t: ");
+        printf("%s \n",employees[i].name);
+ 
+        printf("Age \t: ");
+        printf("%d \n",employees[i].age);
+ 
+        printf("Salary \t: ");
+        printf("%d \n",employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    return 0;
+ 
+}
